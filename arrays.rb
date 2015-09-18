774532e98ec3785e64d11adf7729e7846b2498ae
Michelle Mao <maom@s.dcsdk12.org>
2015-09-18 10:22:33 -0600

    Finish

--- arrays.rb
+++ arrays.rb
@@ -1,5 +1,26 @@
 module Arrays
 	
+	def middles(array1,array2)
+	 	new_array = [array1[1],array2[1]]
+	end
 	
+	def max(array)
+		x = 0
+	 array.each do |element|
+	end
 	
+	def lucky13?(array)
+		sum = 0
+		array.each do |element|
+			if element == 1 || element == 3
+				sum += 1
+			end
+		end
+		if sum == 0
+			true
+		else 
+			false
+		end
+	end
+
 end
--- arrays_test.rb
+++ arrays_test.rb
@@ -10,5 +10,19 @@ class ArraysTest < MiniTest::Test
      include Arrays
    	end.new
 	end
-
+	def test_middles
+		assert_equal [3,4] , @m.middles([1,3,2],[6,4,7])
+		assert_equal [6,7], @m.middles([5,6,7],[4,7,8])
+	end
+	def test_max
+		assert_equal 7, @m.max([1,2,3,4,4,7])
+		assert_equal 1, @m.max([0,0,1])
+		assert_equal 5, @m.max([5])
+	end
+	def test_lucky13
+		assert_equal true, @m.lucky13?([4,5,6,7,8])
+		assert_equal false, @m.lucky13?([1,2,3])
+		assert_equal false, @m.lucky13?([1,4,5])
+		assert_equal false, @m.lucky13?([3,6,7])
+	end
 end
