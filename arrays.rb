b57eb92624128786d9ef009a79912df99e9311f3
Michelle Mao <mm321whatsup@gmail.com>
2015-09-20 15:49:38 -0600

    Finished tests for all the methods and finished the methods

--- arrays.rb
+++ arrays.rb
@@ -2,16 +2,22 @@ module Arrays
 	
 	def middles(array1,array2)
 	 	new_array = [array1[1],array2[1]]
+	 	new_array
 	end
 	
 	def max(array)
-		x = 0
-	 array.each do |element|
+		max = 0
+		array.each do |element|
+			if element > max
+				max = element
+			end
+		end
+		max
 	end
 	
-	def lucky13?(array)
+	def lucky13?(arr)
 		sum = 0
-		array.each do |element|
+		arr.each do |element|
 			if element == 1 || element == 3
 				sum += 1
 			end
@@ -21,6 +27,96 @@ module Arrays
 		else 
 			false
 		end
+	end
+
+	def repeat_seperator(word, seperator, count)
+		new_string = ""
+		(1..count-1).each do |n|
+			new_string += word + seperator
+		end
+		new_string + word
+	end
+	
+	def same_ends?(array , n)
+		for i in 0...n
+		 	if array[i]!= array[array.length-n+i]
+		 		return false
+		 	end
+		 end
+		return true 
+	end
+	
+	def starry?(string)
+		found_star = false
+		for n in 0...string.length
+			if string[n] == "*" 
+				found_star = true
+				if n > 0 && n <= string.length - 2
+					if string[n-1] != string[n +1]
+						return false
+					end
+				end
+			end
+		end
+		if found_star == true
+			true
+		else
+			false
+		end
+	end
+	
+	def plus_it(string, token)
+		new_string = "+" * string.length #for output
+		token_index = 0 # initialize index of the token in the string to zero
+		while true # loop till the string is exhausted(searched entire string)
+			offset = token_index == 0 ? 0 :  token_index + token.length #Start from zero, if we found token index before, find the postiion after the previous index()
+			token_index = string.index(token, offset)# searched the token from the offset in the string(if there is more than 1 token in word)
+			if token_index == nil # if no more tokens found, return new string
+				return new_string
+			else
+				for i in 0...token.length #found one more token or more , put the token back into the new string
+					new_string[token_index + i] = token[i]
+				end
+			end
+			
+		end
+	end
+	
+	def zero_front(array)
+		new_array = []
+		for element in 0...array.length
+			if array[element] == 0
+				new_array.push(array[element])
+			end
+		end
+		for element in 0...array.length
+			if array[element] > 0 
+				new_array.push(array[element])
+			end
+		end
+		new_array
+	end
+	
+	def ten_run(array)
+		multiple = 0
+		for element in 0...array.length
+			multiple = array[element] if array[element] % 10 == 0
+			array[element] = multiple if multiple > 0
+		end
+		array	
+	end
+	
+	def max_span(array)
+		span = 0 # declare the span as zero 
+		for n in 0...array.length # so you're looping through the values
+			for x in n...array.length # and then within each value to the end value
+				if array[x] == array[n] #you're checking if any of the values are equal to each other
+					#once you do, you're going to then subtract the index values, but add 1, since subtracting doesn't count the first 1
+					span = (1 +(x-n)) if (1+(x-n)) > span && x-n >0	#checks whether the span is greater than the last one and that if there is no repition, its 0
+				end
+			end
+		end
+		return span
 	end
 
 end
--- arrays_test.rb
+++ arrays_test.rb
@@ -25,4 +25,46 @@ class ArraysTest < MiniTest::Test
 		assert_equal false, @m.lucky13?([1,4,5])
 		assert_equal false, @m.lucky13?([3,6,7])
 	end
+	def test_repeat_seperator
+		assert_equal "ThisAndThisAndThisAndThis", @m.repeat_seperator("This", "And", 4)
+		assert_equal "YobutYobutYobutYobutYo", @m.repeat_seperator("Yo","but",5)
+		assert_equal "MeepOrMeep", @m.repeat_seperator("Meep","Or",2)
+		assert_equal "Hi", @m.repeat_seperator("Hi", "Because", 1)	
+	end
+	def test_same_ends
+		assert_equal true , @m.same_ends?([5, 6, 45, 99, 23, 5, 6] , 2)
+		assert_equal false, @m.same_ends?([5, 6, 45, 99, 23, 5, 6], 1)
+		assert_equal false, @m.same_ends?([5, 6, 45, 99, 23, 5, 6], 3)
+		assert_equal true, @m.same_ends?([5, 6, 45, 99, 23, 5, 6] , 7)
+	end
+	def test_starry
+		assert_equal true, @m.starry?("me*ep")
+		assert_equal false, @m.starry?("me*rp")
+		assert_equal false, @m.starry?("meep")
+		
+	end
+	def test_plus_it
+		assert_equal "+++ABC+++" , @m.plus_it("123ABC456", "ABC")
+		assert_equal "++++++" , @m.plus_it("123456", 'a')
+		assert_equal "+++abc+++abc" , @m.plus_it("123abc123abc", 'abc')
+		
+	end
+	def test_zero_front
+		assert_equal [0,0,5,3,1,6], @m.zero_front([5,0,3,0,1,6])
+		assert_equal [0,0,0,0,0], @m.zero_front([0,0,0,0,0])
+		assert_equal [5,3,1,6], @m.zero_front([5,3,1,6])
+		assert_equal [0,0,0,0,4,5,6,7,10], @m.zero_front([4,0,0,5,6,7,0,10,0])
+	end
+	def test_ten_run
+		assert_equal [2, 10, 10, 10, 20, 20], @m.ten_run([2, 10, 3, 4, 20, 5])
+		assert_equal [10,20,30,40,50], @m.ten_run([10,20,30,40,50])
+		assert_equal [1,2,3,4,5,6], @m.ten_run([1,2,3,4,5,6])
+		assert_equal [2,10,10,10,30,30], @m.ten_run([2,10,67,84,30,45])
+	end
+	def test_max_span
+		assert_equal 4 , @m.max_span([1,2,1,1,3])
+		assert_equal 6, @m.max_span([1,4,2,1,4,1,4])
+		assert_equal 6, @m.max_span([1,4,2,1,4,4,4])
+   		assert_equal 0, @m.max_span([1,2,3,4,5,6])
+	end
 end
